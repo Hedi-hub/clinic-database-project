bb3d1d9b7166af89abf519ad41358edec21fefc8
Hedi-hub <72658290+Hedi-hub@users.noreply.github.com>
2021-01-19 15:43:16 +0100

    Added first footer decoration plus about us decoration

---
+++ src/main/java/clinicDatabase/controllers/DoctorInquiryController.java
@@ -0,0 +1,20 @@
+package clinicDatabase.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DoctorInquiryController {
+
+    @RequestMapping("/showDoctorInquiry")
+    //WE ADD MODEL BECAUSE IT IS A MSG BOX WHICH COMMUNICATES BTW VARIOUS HTML PAGES
+    public String showDoctorInquiry(@RequestParam("doctor-name") String name,
+                                    Model model){
+
+        model.addAttribute("doctorName", name);
+        return "doctor-inquiry.html";
+    }
+
+}
